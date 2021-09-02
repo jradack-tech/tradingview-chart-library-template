606c3b1560748e077ae698c0203b67483f1a149c
unknown <support@tradingzone.live>
2021-09-02 00:55:02 -0700

    first

---
+++ part2/src/components/TVChartContainer/index.jsx
@@ -0,0 +1,88 @@
+import * as React from 'react';
+import './index.css';
+import Datafeed from './api/'
+import { widget } from '../../charting_library';
+
+
+function getLanguageFromURL() {
+	const regex = new RegExp('[\\?&]lang=([^&#]*)');
+	const results = regex.exec(window.location.search);
+	return results === null ? null : decodeURIComponent(results[1].replace(/\+/g, ' '));
+}
+ 
+export class TVChartContainer extends React.PureComponent {
+
+	static defaultProps = {
+		symbol: 'Coinbase:BTC/USD',
+		interval: '15',
+		containerId: 'tv_chart_container',
+		libraryPath: '/charting_library/',
+		chartsStorageUrl: 'https://saveload.tradingview.com',
+		chartsStorageApiVersion: '1.1',
+		clientId: 'tradingview.com',
+		userId: 'public_user_id',
+		fullscreen: false,
+		autosize: true,
+		studiesOverrides: {},
+	};
+
+	componentDidMount() {
+		const widgetOptions = {
+			debug: false,
+			symbol: this.props.symbol,
+			datafeed: Datafeed,
+			interval: this.props.interval,
+			container_id: this.props.containerId,
+			library_path: this.props.libraryPath,
+			locale: getLanguageFromURL() || 'en',
+			disabled_features: ['use_localstorage_for_settings'],
+			enabled_features: ['study_templates'],
+			charts_storage_url: this.props.chartsStorageUrl,
+			charts_storage_api_version: this.props.chartsStorageApiVersion,
+			client_id: this.props.clientId,
+			user_id: this.props.userId,
+			fullscreen: this.props.fullscreen,
+			autosize: this.props.autosize,
+			studies_overrides: this.props.studiesOverrides,
+			overrides: {
+				"mainSeriesProperties.showCountdown": true,
+				"paneProperties.background": "#131722",
+				"paneProperties.vertGridProperties.color": "#363c4e",
+				"paneProperties.horzGridProperties.color": "#363c4e",
+				"symbolWatermarkProperties.transparency": 90,
+				"scalesProperties.textColor" : "#AAA",
+				"mainSeriesProperties.candleStyle.wickUpColor": '#336854',
+				"mainSeriesProperties.candleStyle.wickDownColor": '#7f323f',
+			}
+		};
+
+		const tvWidget = new widget(widgetOptions);
+		this.tvWidget = tvWidget;
+
+		tvWidget.onChartReady(() => {
+			tvWidget.headerReady().then(() => {
+				const button = tvWidget.createButton();
+				button.setAttribute('title', 'Click to show a notification popup');
+				button.classList.add('apply-common-tooltip');
+				button.addEventListener('click', () => tvWidget.showNoticeDialog({
+					title: 'Notification',
+					body: 'TradingView Charting Library API works correctly',
+					callback: () => {
+						console.log('Noticed!');
+					},
+				}));
+
+				button.innerHTML = 'Check API';
+			});
+		});
+	}
+
+	render() {
+		return (
+			<div
+				id={ this.props.containerId }
+				className={ 'TVChartContainer' }
+			/>
+		);
+	}
+}
